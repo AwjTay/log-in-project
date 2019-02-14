d756d1391ad49fef8dcb69c9e405483f5ffaf9d7
andy <awtaylor86@gmail.com>
2019-02-14 14:17:17 +0000

    session working

--- index.php
+++ index.php
@@ -1,50 +1,62 @@
 <h1>My awesome site!</h1>
 
 <?php
+session_start();
 
-//session_start();
-
-$sub_email = "nigelcrompton@grimshawironworks";
-$sub_password = "goldstandard";
-
-//$validuser = $_SESSION["login"] === true;
-
-// if they have submitted the form
-	// if they have logged in correctly
-		// show a nice miessae
-	// else
-		// show an error
-		// show the form again
-// else
-	//show the form
-$loggedin = false;
+$sub_email = "root@root";
+$sub_password = "root";
 
 
-if ($sub_email === ($_POST['email']) && $sub_password === ($_POST['password'])) {
-	$loggedin = true;
-	?> <p>Welcome</p> <?php 
+//User already logged in
+if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
+	?> <p>Welcome back</p> <?php 
+
 }
 
+//Is the user trying to log in?
+else if ($_POST) {
+	//User logs in with valid userame and password
+	if ($_POST['email'] === $sub_email && $_POST['password'] === $sub_password) {
+		$_SESSION["logged_in"] = true;
+		?> <p>Welcome</p> <?php 
 
+//User is not logged in and submits invalid username/password
+  }	else if ($_SESSION["logged_in"] = false && ($sub_email !== ($_POST['email'])) || ($sub_password !== ($_POST['password']))) {
+
+	?>	<p>Access Denied!</p> 
+
+		<form action="index.php" method="post">
+		<label for="email">Email:</label>
+		<input id="email" type="email" name="email">
+
+		<label for="password">Password:</label>
+		<input id="password" type="password" name="password">
+
+		<input action="" type="submit" name="Log in" value="Log in">
+		</form>
+
+	<?php
+	}
+	else {
+		var_dump($_SESSION);
+	}
+
+
+//$sub_email === $_POST['email'] && $sub_password === $_POST['password']) {
+//	$_SESSION["logged_in"] = true;
+} 
 else {
-	?> <p>Access Denied!</p> 
+	var_dump($_SESSION);
+	?>	<form action="index.php" method="post">
+		<label for="email">Email:</label>
+		<input id="email" type="email" name="email">
 
-	<form action="index.php" method="post">
-	<label for="email">Email:</label>
-	<input id="email" type="email" name="email">
+		<label for="password">Password:</label>
+		<input id="password" type="password" name="password">
 
-	<label for="password">Password:</label>
-	<input id="password" type="password" name="password">
-
-	<input action="" type="submit" name="Log in" value="Log in">
-</form>
-
+		<input action="" type="submit" name="Log in" value="Log in">
+		</form>
 <?php
-
-
-
 }
-
-//(isset($_POST['email']) && isset($_POST['password'])) && 
 ?>
 
