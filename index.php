e3d354c976e938c3d933359e28cfae862ad48cfe
andy <awtaylor86@gmail.com>
2019-02-14 15:08:35 +0000

    logout working but messy

--- index.php
+++ index.php
@@ -6,34 +6,49 @@ session_start();
 $sub_email = "root@root";
 $sub_password = "root";
 
+//var_dump(isset($_POST["logout"]));
 
-//User already logged in
-if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
-	?> <p>Welcome back</p> <?php 
+
+if (isset($_POST["logout"])) {
+	$_SESSION["logged_in"] = false;
+			include('logged_out.html');
 
 }
 
+//User already logged in
+ if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
+	?> <p>Welcome back</p> <?php
+	echo "heloo";
+	include('logout.html');
+
+
+	//user logs out
+ 
+
+
 //Is the user trying to log in?
-else if ($_POST) {
+} else if (isset($_POST["email"]) && isset($_POST["password"]))  {
 	//User logs in with valid userame and password
-	if ($_POST['email'] === $sub_email && $_POST['password'] === $sub_password) {
+	if  ($_POST['email'] === $sub_email && $_POST['password'] === $sub_password) {
 		$_SESSION["logged_in"] = true;
 		?> <p>Welcome</p> <?php 
+		include('logout.html'); 
+
+		//user logs out
+		if (isset($_POST["logout"])) {
+			$_SESSION["logged_in"] = false;
+			include('logged_out.html');
+		}
+
+
 
 //User is not logged in and submits invalid username/password
   }	else if ($_SESSION["logged_in"] = false && ($sub_email !== ($_POST['email'])) || ($sub_password !== ($_POST['password']))) {
 
 	?>	<p>Access Denied!</p> 
-
-		<form action="index.php" method="post">
-		<label for="email">Email:</label>
-		<input id="email" type="email" name="email">
-
-		<label for="password">Password:</label>
-		<input id="password" type="password" name="password">
-
-		<input action="" type="submit" name="Log in" value="Log in">
-		</form>
+		<?php
+		include('form.html'); 
+		?>
 
 	<?php
 	}
@@ -46,17 +61,11 @@ else if ($_POST) {
 //	$_SESSION["logged_in"] = true;
 } 
 else {
-	var_dump($_SESSION);
-	?>	<form action="index.php" method="post">
-		<label for="email">Email:</label>
-		<input id="email" type="email" name="email">
-
-		<label for="password">Password:</label>
-		<input id="password" type="password" name="password">
-
-		<input action="" type="submit" name="Log in" value="Log in">
-		</form>
-<?php
+	//var_dump($_SESSION);
+	include('form.html'); 
+		
 }
+
+//var_dump($_POST)
 ?>
 
